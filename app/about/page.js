fc16da8331e9b509cd1f034941a7c0119941ca84
Shrey Vijay <shreyvijay2004@gmail.com>
2025-06-11 21:45:17 +0530

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -2,16 +2,17 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex flex-col gap-4 pl-20 pr-150 py-10">
-      <p className="text-3xl font-bold ">About</p>
-      <p className="pl-5">
-        Your go-to URL Shortener. The most straightforward URL shortener — no
-        sign-ups, no ads, no hassle. Just paste your long link and get a short
-        one instantly. It&apos;s fast, simple, and free. Whether you&apos;re sharing for
-        work, social media, or anything in between, we make link shortening
-        effortless.
-      </p>
-    </div>
+    <div className="flex flex-col gap-4 pl-20 pr-[150px] py-10 max-w-4xl">
+  <p className="text-3xl font-bold">About</p>
+  <p className="pl-5">
+    Your go-to URL Shortener. The most straightforward URL shortener — no
+    sign-ups, no ads, no hassle. Just paste your long link and get a short
+    one instantly. It&apos;s fast, simple, and free. Whether you&apos;re sharing for
+    work, social media, or anything in between, we make link shortening
+    effortless.
+  </p>
+</div>
+
   );
 };
 
